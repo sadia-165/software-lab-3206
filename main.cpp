2d7a7a9d846608058d29847bc9c398ff5093bd73
Sadia Hoque <sadiahoque2004@gmail.com>
2026-07-27 11:45:22 +0600

    Feat: Add a new calculation function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Sum = " << add(10, 20);
+
     return 0;
 }
