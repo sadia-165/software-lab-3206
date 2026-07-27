e74096d1b2e78ad7a2025d582c0032ccd1a5c6ab
Sadia-165 <sadia2004@gmail.com>
2026-07-27 12:18:39 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b)
-{
-    return a + b;
-}
-
 int main()
 {
-    cout << "Hello World" << endl;
-    cout << "Sum = " << add(10, 20);
-
+    cout << "Hello Software Engineering Lab" << endl;
     return 0;
 }
